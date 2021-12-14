aac1d4108923ae634a3ac46b37c06f435c94c8da
404kevnotfound <89538364+kmart-tech@users.noreply.github.com>
2021-12-14 13:51:27 -0700

    Add chapter 20 exercises

---
+++ chapter20/Exercise08_37.java
@@ -0,0 +1,88 @@
+/*
+Kevin Martinsen
+CSCI 1116 - Algorithms and Data Structure
+12/14/2021
+
+Exercise 08_37 - Guess the capital game with random order
+*/
+
+import java.util.Scanner;import java.util.*;
+
+public class Exercise08_37 {
+  public static void main(String[] args) {
+    String[][] stateCapital = {
+      {"Alabama", "Montgomery"},
+      {"Alaska", "Juneau"},
+      {"Arizona", "Phoenix"},
+      {"Arkansas", "Little Rock"},
+      {"California", "Sacramento"},
+      {"Colorado", "Denver"},
+      {"Connecticut", "Hartford"},
+      {"Delaware", "Dover"},
+      {"Florida", "Tallahassee"},
+      {"Georgia", "Atlanta"},
+      {"Hawaii", "Honolulu"},
+      {"Idaho", "Boise"},
+      {"Illinois", "Springfield"},
+      {"Indiana", "Indianapolis"},
+      {"Iowa", "Des Moines"},
+      {"Kansas", "Topeka"},
+      {"Kentucky", "Frankfort"},
+      {"Louisiana", "Baton Rouge"},
+      {"Maine", "Augusta"},
+      {"Maryland", "Annapolis"},
+      {"Massachusettes", "Boston"},
+      {"Michigan", "Lansing"},
+      {"Minnesota", "Saint Paul"},
+      {"Mississippi", "Jackson"},
+      {"Missouri", "Jefferson City"},
+      {"Montana", "Helena"},
+      {"Nebraska", "Lincoln"},
+      {"Nevada", "Carson City"},
+      {"New Hampshire", "Concord"},
+      {"New Jersey", "Trenton"},
+      {"New York", "Albany"},
+      {"New Mexico", "Santa Fe"},
+      {"North Carolina", "Raleigh"},
+      {"North Dakota", "Bismarck"},
+      {"Ohio", "Columbus"},
+      {"Oklahoma", "Oklahoma City"},
+      {"Oregon", "Salem"},
+      {"Pennsylvania", "Harrisburg"},
+      {"Rhode Island", "Providence"},
+      {"South Carolina", "Columbia"},
+      {"South Dakota", "Pierre"},
+      {"Tennessee", "Nashville"},
+      {"Texas", "Austin"},
+      {"Utah", "Salt Lake City"},
+      {"Vermont", "Montpelier"},
+      {"Virginia", "Richmond"},
+      {"Washington", "Olympia"},
+      {"West Virginia", "Charleston"},
+      {"Wisconsin", "Madison"},
+      {"Wyoming", "Cheyenne"}
+    };
+    
+    ArrayList<String[]> randomOrder = new ArrayList<String[]>(Arrays.asList(stateCapital));
+        
+    Collections.shuffle(randomOrder);
+
+    Scanner input = new Scanner(System.in);
+    
+    int correctCount = 0;
+    
+    for (String[] pair: randomOrder) {
+      System.out.print("What is the capital of " + pair[0] + "? ");
+      String capital = input.nextLine().trim().toLowerCase();
+      
+      if (capital.equals(pair[1].toLowerCase())) {
+        System.out.println("Your answer is correct");
+        correctCount++;
+      }
+      else
+        System.out.println("The correct answer should be " + pair[1]);
+    }
+    
+    System.out.println("The correct count is " + correctCount);
+  }
+}
---
+++ chapter20/Exercise20_21/Exercise20_21.java
@@ -0,0 +1,40 @@
+/*
+Kevin Martinsen
+CSCI 1116 - Algorithms and Data Structures
+12/14/2021
+
+Exercise 20_21 - generic selection sort using a comparator
+*/
+import java.util.Comparator;
+
+public class Exercise20_21 {
+  public static void main(String[] args) {
+    GeometricObject[] list = {new Circle(5), new Rectangle(4, 5),
+        new Circle(5.5), new Rectangle(2.4, 5), new Circle(0.5), 
+        new Rectangle(4, 65), new Circle(4.5), new Rectangle(4.4, 1),
+        new Circle(6.5), new Rectangle(4, 5)};
+
+    Circle[] list1 = {new Circle(2), new Circle(3), new Circle(2),
+      new Circle(5), new Circle(6), new Circle(1), new Circle(2),
+      new Circle(3), new Circle(14), new Circle(12)};
+    selectionSort(list1, new GeometricObjectComparator());
+    for (int i = 0; i < list1.length; i++)
+      System.out.println(list1[i].getArea() + " ");
+  }
+  
+  public static <E> void selectionSort(E[] list, Comparator<? super E> comparator) {
+    for (int i = 0; i < list.length; i++) {
+      E currentMin = list[i];
+      int swapPos = i;
+      for (int j = i; j < list.length; j++) {
+        if (comparator.compare(list[j],currentMin) < 0) {
+          currentMin = list[j];
+          swapPos = j;
+        }
+      }
+      E temp = list[i];
+      list[i] = currentMin;
+      list[swapPos] = temp;
+    }
+  }
+}
