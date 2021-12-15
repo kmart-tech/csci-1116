8746dcad8f66c3c02a76f45b93604d4989c1b1a8
kmart-tech <89538364+kmart-tech@users.noreply.github.com>
2021-12-15 16:26:36 -0700

    Add Exercise 24_01

---
+++ chapter24/Exercise24_01/Exercise24_01.java
@@ -0,0 +1,41 @@
+/*
+Kevin Martinsen
+CSCI 1116 - Algorithms and Data Structure
+12/15/2021
+
+Exercise 24_1 - MyList implementation
+ */
+
+import java.util.*;
+
+public class Exercise24_01 {
+  public static void main(String[] args) {
+    new Exercise24_01();
+  }
+
+  public Exercise24_01() {
+    String[] name1 = {"Tom", "Tom", "George", "Peter", "Jean", "Jane"};
+    String[] name2 = {"Tom", "George", "Michael", "Michelle", "Daniel"};
+    
+    MyList<String> list1 = new MyArrayList<String>(name1);   
+    MyList<String> list2 = new MyArrayList<String>(name2);   
+    System.out.println("list1:" + list1);
+    System.out.println("list2:" + list2);
+    list1.addAll(list2);
+    System.out.println("After addAll:" + list1 + "\n");
+    
+    list1 = new MyArrayList<String>(name1);
+    list2 = new MyArrayList<String>(name2);   
+    System.out.println("list1:" + list1);
+    System.out.println("list2:" + list2);
+    list1.removeAll(list2);
+    System.out.println("After removeAll:" + list1 + "\n");
+    
+    list1 = new MyArrayList<String>(name1);
+    list2 = new MyArrayList<String>(name2);   
+    System.out.println("list1:" + list1);
+    System.out.println("list2:" + list2);
+    list1.retainAll(list2);
+    System.out.println("After retainAll:" + list1 + "\n");
+  }
+}
---
+++ chapter24/Exercise24_01/MyAbstractList.java
@@ -0,0 +1,86 @@
+/*
+Kevin Martinsen
+CSCI 1116 - Algorithms and Data Structure
+12/15/2021
+
+Exercise 24_1 - MyList implementation
+ */
+public abstract class MyAbstractList<E> implements MyList<E> {
+	protected int size = 0; // The size of the list
+
+	/** Create a default list */
+	protected MyAbstractList() {
+	}
+
+	/** Create a list from an array of objects */
+	protected MyAbstractList(E[] objects) {
+		for (int i = 0; i < objects.length; i++)
+			add(objects[i]);
+	}
+
+	/** Add a new element at the end of this list */
+	public void add(E e) {
+		add(size, e);
+	}
+
+	/** Return true if this list contains no elements */
+	public boolean isEmpty() {
+		return size == 0;
+	}
+
+	/** Return the number of elements in this list */
+	public int size() {
+		return size;
+	}
+
+	/** Remove the first occurrence of the element o from this list.
+	 *  Shift any subsequent elements to the left.
+	 *  Return true if the element is removed. */
+	public boolean remove(E e) {
+		if (indexOf(e) >= 0) {
+			remove(indexOf(e));
+			return true;
+		}
+		else 
+			return false;
+	}
+
+	public boolean addAll(MyList<E> otherList) {
+		boolean temp = otherList.isEmpty();
+		if (!otherList.isEmpty()) {
+			for (int i = 0; i < otherList.size(); i++) {
+				add(otherList.get(i));
+			}
+			return true;
+		}
+		else return false;
+	}
+
+	public boolean removeAll(MyList<E> otherList) {
+		boolean changed = false;
+		if (!otherList.isEmpty()) {
+			for (int i = 0; i < otherList.size(); i++) {
+				while(remove(otherList.get(i))) { //use a while loop to remove all instances of the element
+					changed = true;
+				}
+			}
+		}
+		return changed;
+	}
+
+	public boolean retainAll(MyList<E> otherList) {
+		boolean changed = false;
+		if (otherList.isEmpty()) {
+			clear();
+			changed = true;
+		}
+		else {
+			for (int i = 0; i < size(); i++) {
+				if (!otherList.contains(get(i))) {
+					remove(i);
+				}
+			}
+		}
+		return changed;
+	}
+}
