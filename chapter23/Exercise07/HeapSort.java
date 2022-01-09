7613ade8b40bfb95d3883289ff56b21fb34ca7e4
kmart-tech <martins.kev3886@stu.stech.edu>
2022-01-09 12:27:41 -0700

    Add exercise 23_07

---
+++ chapter23/Exercise07/HeapSort.java
@@ -0,0 +1,30 @@
+/*
+Kevin Martinsen
+CSCI 1116 - Algorithms and Data Structures
+1/9/22
+
+Exercise 23_7 - Min-heap
+ */
+public class HeapSort {
+	/** Heap sort method */
+	public static <E extends Comparable<E>> void heapSort(E[] list) {
+		// Create a Heap of integers
+		Heap<E> heap = new Heap<>();
+
+		// Add elements to the heap
+		for (int i = 0; i < list.length; i++)
+			heap.add(list[i]);
+
+		// Remove elements from the heap
+		for (int i = list.length - 1; i >= 0; i--)
+			list[i] = heap.remove();
+	}
+	
+	/** A test method */
+	public static void main(String[] args) {
+		Integer[] list = {-44, -5, -3, 3, 3, 1, -4, 0, 1, 2, 4, 5, 53}; 
+		heapSort(list);
+		for (int i = 0; i < list.length; i++)
+			System.out.print(list[i] + " ");
+	}
+}
